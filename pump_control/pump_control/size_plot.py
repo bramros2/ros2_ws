dc1ecfd6d6625b7d3df4590c8e7dcda617849463
Bram Oudenbroek <bram.oudenbroek@wur.nl>
2023-03-23 10:56:41 +0100

    23-3 Fix size_plot.py, should update plot after recieving more data

--- pump_control/pump_control/size_plot.py
+++ pump_control/pump_control/size_plot.py
@@ -2,49 +2,45 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float64
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-
 
 
 class DropletSizeSubscriber(Node):
 
     def __init__(self):
         super().__init__('droplet_size_subscriber')
-        print('Node initialized')
-
+        self.line = None
         self.droplet_sizes = []
         self.times = []
-        fig, ax = plt.subplots()
+        self.init_plot()
+        print('Node initialized')
 
         self.subscription = self.create_subscription(
             Float64,
             'droplet_size',
-            lambda msg: self.droplet_size_callback(msg, fig, ax),
+            lambda msg: self.droplet_size_callback(msg),
             10)
         self.subscription  # prevent unused variable warning
 
-        
+    def init_plot(self):
 
-        
-    def droplet_size_callback(self, msg,fig,ax):
+        self.fig, self.ax = plt.subplots()
+        self.line, = self.ax.plot(self.times, self.droplet_sizes)
+        plt.show(block = False)
+
+    def droplet_size_callback(self, msg):
         droplet_size = msg.data
         now = self.get_clock().now().to_msg().sec
         self.droplet_sizes.append(droplet_size)
         self.times.append(now)
         print(self.droplet_sizes[-1])
-        
 
         # Update the plot
-        ax.clear()
-        
-        ax.plot(self.times,self.droplet_sizes)
-        ax.set_title('Droplet size vs Time')
-        ax.set_ylabel('Droplet size in pixels')
-
-        ax.relim()
-        ax.autoscale_view()
-
-        fig.canvas.draw()
+        self.line.set_xdata(self.times)
+        self.line.set_ydata(self.droplet_sizes)
+        self.ax.relim()
+        self.ax.autoscale_view
+        self.fig.canvas.draw_idle()
+        self.fig.canvas.flush_events()
 
 def main(args=None):
     rclpy.init(args=args)
